4070b6dfca8daa055dcdddf78ca9106378231ee1
Bhanuvamsi2005 <bhanuvamsia@gmail.com>
2025-08-11 10:59:43 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://184.72.122.226:9090/springapp1';
+const BASE_URL = 'http://16.170.227.174:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
